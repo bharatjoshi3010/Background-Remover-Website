e76c5823b3ceb372c7eef9a680bb56b1f2d9650b
unknown <bj20032003@gmail.com>
2026-02-14 22:26:33 +0530

    created transactionSchema

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,6 @@
-import {Webhook} from 'svix'
+import {messageInRaw, Webhook} from 'svix'
 import userModel from '../models/userModel.js'
+import razorpay from 'razorpay'
 
 //API controller funtion to manage clerk user with database
 //http://localhost:4000/api/user/webhooks
@@ -85,5 +86,55 @@ const userCredits = async (req, res) => {
         res.json({success:false, message:error.message})
     }
 }
+
+// Gateway initilize
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+})
+
+//API to make payment for credits
+const paymentRazorpay = async (req, res) => {   
+    try {
+
+        const { clerkId, planId } = req
+
+        const userData = await userModel.findOne({ clerkId })
+
+        if(!userData || !planId){
+            return res.json({ success: false, message:'Invalid Credentials'})
+        }
+
+        let credits, plan, amount, date 
+
+        switch (planId){
+            case 'Basic':
+                plan = "Basic"
+                credits = 100
+                amount = 10
+                break;
+            case 'Advanced':
+                plan = "Advanced"
+                credits = 500
+                amount = 50
+                break;
+            case 'Business':
+                plan = "Business"
+                credits = 5000
+                amount = 250
+                break;
+
+            default:
+                break;
+        }
+
+        date = Date.now()
+        
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
 
 export {clerkWebhooks, userCredits}
